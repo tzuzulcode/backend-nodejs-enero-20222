0ed12b2b9ad171a6c74b4e78472f6f0c8f7863d0
Tzuzul Code <mail@tzuzulcode.com>
2022-01-20 09:05:09 -0600

    Definicion de rutas y body parsing

--- Movies/index.js
+++ Movies/index.js
@@ -2,8 +2,19 @@ const express = require("express")
 
 const app = express()
 
+//Usando middleware
+app.use(express.text())
+app.use(express.json())
+
 app.get('/',(req,res)=>{
     return res.status(200).send('Hola, bienvenido')
+})
+app.get('/otraruta',(req,res)=>{
+    return res.status(200).send('Hola, otra ruta')
+})
+app.post('/guardar',(req,res)=>{
+    console.log(req.body)
+    return res.status(200).send('Hola, guardar')
 })
 
 app.listen(4000,()=>{

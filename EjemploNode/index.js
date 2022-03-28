632c5717a55b4f03d658a180a1e35f8efc85a2f0
Tzuzul Code <mail@tzuzulcode.com>
2022-03-28 08:10:48 -0600

    Proyecto terminado

--- EjemploNode/index.js
+++ EjemploNode/index.js
@@ -3,9 +3,21 @@ const http = require('http')
 const server = http.createServer()
 
 server.on('request',(request,response)=>{
-    response.statusCode = '200'
-    response.setHeader('Content-Type','text-plain')
-    response.end('Hola, bienvenido')
+    if(request.method==="POST" && request.url=="/stream"){
+        let body = []
+        request.on("data",(chunk)=>{
+            body.push(chunk)
+        })
+        .on("end",()=>{
+            response.writeHead(200,{"Content-Type":"application/json"})
+            body = Buffer.concat(body).toString()
+            response.end(body)
+        })
+    }else{
+        response.statusCode = '200'
+        response.setHeader('Content-Type','text-plain')
+        return response.end('Hola, bienvenido')
+    }
 })
 
 server.listen(4000)
---
+++ EjemploNode/streams.js
@@ -0,0 +1,11 @@
+// Writable stream
+const {Writable} = require("stream")
+
+const writableStream = new Writable({
+    write(chunk,encoding,callback){
+        console.log(chunk.toString())
+        callback()
+    }
+})
+
+process.stdin.pipe(writableStream)

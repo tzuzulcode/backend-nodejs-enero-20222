3f7a968efb9b994018b668cd619c35da3cbe68f9
Tzuzul Code <mail@tzuzulcode.com>
2022-03-30 09:17:03 -0600

    Pago completado

--- ecommerce/backend/routes/payments.js
+++ ecommerce/backend/routes/payments.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const Payments = require("../services/payments")
 
+
+
 function payments(app){
     const router = express.Router()
 
@@ -15,6 +17,21 @@ function payments(app){
         return res.json({
             clientSecret:intent
         })
+    })
+
+    router.post("/webhook",express.raw({type: 'application/json'}),async(req,res)=>{
+        const sig = req.headers['stripe-signature'];
+
+        console.log(req.body)
+
+        const result = pay.createEvent(req.body,sig)
+        console.log(result)
+        if(result.success){
+
+            return res.status(200).send()
+        }
+
+        return res.status(400).send();
     })
 }
 
--- ecommerce/backend/services/payments.js
+++ ecommerce/backend/services/payments.js
@@ -1,6 +1,7 @@
 const { stripe_sk } = require("../config");
 
 const stripe = require("stripe")(stripe_sk)
+const endpointSecret = "whsec_5a3835bc94d54c23aefcaab36ea471c6fe46a38fb1f18e087c95a0b21398dd29 "
 
 class Payments{
     async createIntent(amount){
@@ -10,6 +11,22 @@ class Payments{
         })
 
         return intent.client_secret
+    }
+
+
+    createEvent(body,sign){
+        let event;
+
+        const payloadString = JSON.stringify(body, null, 2);
+
+        try {
+            event = stripe.webhooks.constructEvent(payloadString, sign, endpointSecret);
+        } catch (err) {
+            return {success:false,message:`Webhook Error: ${err.message}`}
+
+        }
+
+        return {success:true,event}
     }
 }
 

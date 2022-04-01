788a1c4d2ab392080f1de275a11ef22d1b828959
Tzuzul Rosas <tzuzulrosas@gmail.com>
2022-04-01 09:02:36 -0600

    Pago con stripe

--- ecommerce/backend/config/index.js
+++ ecommerce/backend/config/index.js
@@ -1,11 +1,11 @@
-require("../../../WorkflowApp/backend/node_modules/dotenv/lib/main").config()
+require("dotenv").config()
 
 const config = {
-    env:process.env.NODE_ENV,
-    port:process.env.PORT,
-    jwt_secret:process.env.JWT_SECRET,
-    stripe_sk:process.env.STRIPE_SK,
-    stripe_pk:process.env.STRIPE_PK,
+    env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    jwt_secret: process.env.JWT_SECRET,
+    stripe_sk: process.env.STRIPE_SK,
+    stripe_pk: process.env.STRIPE_PK,
 }
 
 module.exports = config
--- ecommerce/backend/services/payments.js
+++ ecommerce/backend/services/payments.js
@@ -1,34 +1,34 @@
 const { stripe_sk } = require("../config");
 
 const stripe = require("stripe")(stripe_sk)
-const endpointSecret = "whsec_5a3835bc94d54c23aefcaab36ea471c6fe46a38fb1f18e087c95a0b21398dd29"
+const endpointSecret = "whsec_2d849de04e6aa72abd49bf02b669777334504a448b75a97e166203f8fb714ffe"
 
-class Payments{
-    async createIntent(amount){
+class Payments {
+    async createIntent(amount) {
         const intent = await stripe.paymentIntents.create({
             amount: amount,//price
-            currency:"usd"
+            currency: "usd"
         })
 
         return intent.client_secret
     }
 
 
-    createEvent(body,sign){
+    createEvent(body, sign) {
         let event;
 
         try {
             event = stripe.webhooks.constructEvent(body, sign, endpointSecret);
 
-            if(event.type==="payment_intent.succeeded"){
+            if (event.type === "payment_intent.succeeded") {
                 console.log(event.data)
-                return {success:true}
-            }else{
-                return {success:true}
+                return { success: true }
+            } else {
+                return { success: true }
             }
-            
+
         } catch (err) {
-            return {success:false,message:`Webhook Error: ${err.message}`}
+            return { success: false, message: `Webhook Error: ${err.message}` }
 
         }
     }

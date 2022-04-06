cf8664cf94919954fb710ced98cebd883ea2c015
Tzuzul Rosas <tzuzulrosas@gmail.com>
2022-04-06 07:15:14 -0500

    Microservicio

--- ecommerce/backend/config/index.js
+++ ecommerce/backend/config/index.js
@@ -4,6 +4,16 @@ const config = {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
     jwt_secret: process.env.JWT_SECRET,
+    db_password: process.env.DB_PASSWORD,
+    db_username: process.env.DB_USERNAME,
+    db_host: process.env.DD_HOST,
+    db_name: process.env.DB_NAME,
+    callback_url: process.env.NODE_ENV === "dev" ?
+        process.env.CALLBACK_URL_DEVELOPMENT + ":" + process.env.PORT :
+        process.env.CALLBACK_URL,
+    oauth_client_id: process.env.OAUTH_CLIENT_ID,
+    oauth_client_secret: process.env.OAUTH_CLIENT_SECRET,
+    bucket_name: process.env.BUCKET_NAME,
     stripe_sk: process.env.STRIPE_SK,
     stripe_pk: process.env.STRIPE_PK,
 }
--- ecommerce/backend/index.js
+++ ecommerce/backend/index.js
@@ -1,29 +1,34 @@
 const express = require("express")
 const cors = require("cors")
 const { port, env } = require("./config")
+const { connection } = require("./config/db")
+
+connection()
 
 
 const payments = require("./routes/payments")
+const products = require("./routes/products")
 
 
 const app = express()
 
-app.use("/api/payments/webhook",express.raw({type: 'application/json'}))
+app.use("/api/payments/webhook", express.raw({ type: 'application/json' }))
 app.use(express.json())
 app.use(cors({
-    credentials:true,
-    origin:["http://locahost:3000","http://localhost:63342","http://127.0.0.1:5500"]
+    credentials: true,
+    origin: ["http://locahost:3000", "http://localhost:63342", "http://127.0.0.1:5500"]
 }))
 
 
 payments(app)
+products(app)
 
 
-app.get("/",(req,res)=>{
-    return res.json({hello:"world"})
+app.get("/", (req, res) => {
+    return res.json({ hello: "world" })
 })
 
-app.listen(port,()=>{
-    console.log("Modo:",env)
-    console.log("listening on: http://localhost:"+port)
+app.listen(port, () => {
+    console.log("Modo:", env)
+    console.log("listening on: http://localhost:" + port)
 })
